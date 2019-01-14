4e8f2f8c3a74cace3455e485767e8fbd12be3906
Peter Kehl <peter.kehl@gmail.com>
2019-01-13 23:40:21 -0800

    Description

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sub-context "0.1.0-SNAPSHOT"
+(defproject subcontext "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
---
+++ src/subcontext/reader.clj
@@ -0,0 +1,1 @@
+(ns subcontext.reader)
